caf3cf9f293e4dff3e37952d688977b88d3727fd
Richard Price <mrspondooli@gmail.com>
2021-01-26 21:52:39 +0000

    initial commit

---
+++ TinderClone/TinderClone/MainController.swift
@@ -0,0 +1,17 @@
+//
+//  MainController.swift
+//  TinderClone
+//
+//  Created by Richard Price on 26/01/2021.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+        
+    }
+}
